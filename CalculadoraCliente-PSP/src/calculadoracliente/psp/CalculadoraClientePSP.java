077378e3c130698c9f4cc7df855576b6ff6e58e6
David <David@192.168.1.106>
2019-02-04 17:46:57 +0100

    Terminado

---
+++ CalculadoraCliente-PSP/src/calculadoracliente/psp/CalculadoraClientePSP.java
@@ -0,0 +1,20 @@
+
+package calculadoracliente.psp;
+
+/**
+ *
+ * @author David
+ */
+public class CalculadoraClientePSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        InterfazCalculadora in=new InterfazCalculadora();
+        in.setVisible(true);
+        
+    }
+    
+}

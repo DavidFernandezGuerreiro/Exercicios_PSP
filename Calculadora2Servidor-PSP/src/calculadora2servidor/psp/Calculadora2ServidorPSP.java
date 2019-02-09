cb734cf78d27c4654398e7f8e73a07963efd4120
David <David@192.168.1.106>
2019-02-09 11:37:03 +0100

    Terminado

---
+++ Calculadora2Servidor-PSP/src/calculadora2servidor/psp/Calculadora2ServidorPSP.java
@@ -0,0 +1,20 @@
+
+package calculadora2servidor.psp;
+
+/**
+ *
+ * @author oracle
+ */
+public class Calculadora2ServidorPSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Metodos_Servidor m=new Metodos_Servidor();
+        m.conexion();
+
+    }
+    
+}

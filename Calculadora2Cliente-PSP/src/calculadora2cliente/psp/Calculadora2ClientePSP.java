3c616ed27d15b454ab6becd1a8668f7838e60c4f
David <David@192.168.1.106>
2019-02-09 11:36:48 +0100

    Terminado

---
+++ Calculadora2Cliente-PSP/src/calculadora2cliente/psp/Calculadora2ClientePSP.java
@@ -0,0 +1,22 @@
+
+package calculadora2cliente.psp;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author oracle
+ */
+public class Calculadora2ClientePSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        InterfazCalculadora in=new InterfazCalculadora();
+        in.setVisible(true);
+        
+    }
+    
+}

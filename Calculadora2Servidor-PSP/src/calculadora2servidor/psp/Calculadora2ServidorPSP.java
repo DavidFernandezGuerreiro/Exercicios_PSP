7c8554ae62ff9b6a71164a04629828195cdf7f1a
David <David@192.168.1.106>
2019-02-16 11:39:06 +0100

    Mejorado, comentado y terminado

--- Calculadora2Servidor-PSP/src/calculadora2servidor/psp/Calculadora2ServidorPSP.java
+++ Calculadora2Servidor-PSP/src/calculadora2servidor/psp/Calculadora2ServidorPSP.java
@@ -3,7 +3,7 @@ package calculadora2servidor.psp;
 
 /**
  *
- * @author oracle
+ * @author David Fernández
  */
 public class Calculadora2ServidorPSP {
 
@@ -12,7 +12,9 @@ public class Calculadora2ServidorPSP {
      */
     public static void main(String[] args) {
         
+        //Declaro un objeto de la clase Metodos_Servidor
         Metodos_Servidor m=new Metodos_Servidor();
+        //Desde el objeto llamo al método conexión
         m.conexion();
 
     }

ed855a4fb1a0e9dff205fd9f0080f55f59afcdeb
David <David@192.168.1.106>
2019-03-15 15:51:50 +0100

    Comentado y terminado

---
+++ ChatServidorPSP/src/chatservidorpsp/ChatServidorPSP.java
@@ -0,0 +1,22 @@
+
+package chatservidorpsp;
+
+/**
+ *
+ * @author David
+ */
+public class ChatServidorPSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Declaro un objeto de la clase MetodosServidor
+        MetodosServidor m=new MetodosServidor();
+        //desde el objeto llamo al método "conexion"
+        m.conexion();
+        
+    }
+    
+}

e40602c5fc0193c167dd5a69a5cfc45b054a3e5c
David <David@192.168.1.106>
2019-03-15 15:51:29 +0100

    Comentado y terminado

---
+++ ChatClientePSP/src/chatclientepsp/ChatClientePSP.java
@@ -0,0 +1,22 @@
+
+package chatclientepsp;
+
+/**
+ *
+ * @author David
+ */
+public class ChatClientePSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Declaro un objeto de la clase JFrame Form llamada InterfazCliente
+        InterfazCliente in=new InterfazCliente();
+        //desde el objeto hago visible la interfaz
+        in.setVisible(true);
+        
+    }
+    
+}
